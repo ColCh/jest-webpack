8da205226d07d781568c913b209033587be11dc7
ColCh <colchgh@gmail.com>
2015-04-06 13:52:03 +0300

    Add test for sum.coffee within jest test

--- test/fixtures/jest_tests/__tests__/sum-test.js
+++ test/fixtures/jest_tests/__tests__/sum-test.js
@@ -1,9 +1,19 @@
 // __tests__/sum-test.js
-jest.dontMock('../sum');
+jest.dontMock('../sum.js');
+jest.dontMock('../sum.coffee');
 
-describe('sum', function() {
- it('adds 1 + 2 to equal 3', function() {
-   var sum = require('../sum');
-   expect(sum(1, 2)).toBe(3);
- });
+describe('sum', function () {
+  describe('js', function() {
+   it('adds 1 + 2 to equal 3', function() {
+     var sum = require('../sum.js');
+     expect(sum(1, 2)).toBe(3);
+   });
+  });
+
+  describe('coffee', function() {
+    it('adds 1 + 2 to equal 3', function() {
+      var sum = require('../sum.coffee');
+      expect(sum(1, 2)).toBe(3);
+    });
+  });
 });
